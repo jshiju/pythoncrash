363dbf5c49458fa7ff5bef3a2f01044537ec45d9
Shiju John <Shiju.John1@ibm.com>
2023-10-21 11:51:26 +0530

    Moving forwrd

---
+++ ch02/03_whitespace.py
@@ -0,0 +1,6 @@
+
+print("Languages:\nPython\nC\nJavaScript")
+
+print("\n\n")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
---
+++ ch02/04_strip.py
@@ -0,0 +1,7 @@
+fav_lang = ' python '
+
+print("Before: '" + fav_lang + "'")
+fav_lang = fav_lang.lstrip()
+fav_lang = fav_lang.strip()
+
+print("After: '" + fav_lang + "'")
