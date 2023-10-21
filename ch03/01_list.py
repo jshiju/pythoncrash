8dec573652e4d9f9d0b1375f67a3760fb4c49731
Shiju John <Shiju.John1@ibm.com>
2023-10-21 14:13:57 +0530

    Moving forward

---
+++ ch03/01_list.py
@@ -0,0 +1,12 @@
+names = ["Mark", "George", "Sam", "Jacob"]
+
+
+print(names)
+
+print(names[3])
+
+# reverse
+print(names[-3])
+
+# IndexError: list index out of range
+# print(names[-5])
---
+++ ch03/02_complist.py
@@ -0,0 +1,7 @@
+
+# hetrogeneous elements
+listheader = ["Name", "Age", "Income(m)"]
+list = ["Sam", 35, 25.65]
+
+print(listheader)
+print(list)
