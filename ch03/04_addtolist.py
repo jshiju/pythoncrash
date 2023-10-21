3329989ec9b36d9f9e798ca390f1931f36424afa
Shiju John <Shiju.John1@ibm.com>
2023-10-21 14:19:26 +0530

    Moving forward

--- ch03/04_addtolist.py
+++ ch03/04_addtolist.py
@@ -3,3 +3,13 @@ print(motorcycles)
 
 motorcycles.append('ducati')
 print(motorcycles)
+
+# append - list added as another 'item' in the initial list
+bikes = ["ola", "ather"]
+motorcycles.append(bikes)
+print(motorcycles)
+
+# extend - spread items and add to the initial list
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.extend(bikes)
+print(motorcycles)
