0ce4bc6e5a7091f661933780dcb065301dfb9b61
Shiju John <Shiju.John1@ibm.com>
2023-10-28 21:23:51 +0530

    Started gaming...

--- proj_alien/alien_invasion.py
+++ proj_alien/alien_invasion.py
@@ -4,7 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
-
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -23,16 +23,24 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
-        # Set the background color.
-        # self.bg_color = (230, 230, 230)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+            self._remove_bullets()
             self._update_screen()
             self.clock.tick(60)
+
+    def _remove_bullets(self):
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        #print(len(self.bullets))
 
     def _check_events(self):
         # Watch for keyboard and mouse events.
@@ -67,10 +75,20 @@ class AlienInvasion:
             self.ship.moving_down = False
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            for bullet in self.bullets.sprites():
+                bullet.draw_bullet()
             self.ship.blitme()
             # Make the most recently drawn screen visible.
             pygame.display.flip()
--- proj_alien/settings.py
+++ proj_alien/settings.py
@@ -9,3 +9,11 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         self.ship_speed = 2.5
+
+        # Bullet settings
+        self.bullet_speed = 5.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
+        self.bullets_allowed = 5
